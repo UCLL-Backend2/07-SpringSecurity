c73ecb9796a1654f9bc138d9bb942bee774c69d6
Roel Standaert <roel.standaert@ucll.be>
2024-10-17 13:48:51 +0200

    RBAC

---
+++ src/main/java/be/ucll/backend2/springsec/controller/UserController.java
@@ -0,0 +1,30 @@
+package be.ucll.backend2.springsec.controller;
+
+import be.ucll.backend2.springsec.entity.User;
+import be.ucll.backend2.springsec.service.UserService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable long id,
+                           @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+}
--- src/main/java/be/ucll/backend2/springsec/service/UserService.java
+++ src/main/java/be/ucll/backend2/springsec/service/UserService.java
@@ -4,8 +4,14 @@ import be.ucll.backend2.springsec.controller.dto.RegisterUserDto;
 import be.ucll.backend2.springsec.entity.Role;
 import be.ucll.backend2.springsec.entity.User;
 import be.ucll.backend2.springsec.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -26,6 +32,30 @@ public class UserService {
                 registerUserDto.lastName(),
                 Role.User
         );
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(long id, User userUpdate) {
+        final var authentication = SecurityContextHolder.getContext().getAuthentication();
+        final var isAdmin = authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        final var user = userRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new EntityNotFoundException("User with id " + id + " not found"));
+        if (!isAdmin && !user.getEmailAddress().equals(authentication.getName())) {
+            throw new AccessDeniedException("Not allowed to change user with id " + id);
+        }
+        user.setEmailAddress(userUpdate.getEmailAddress());
+        user.setPassword(passwordEncoder.encode(userUpdate.getPassword()));
+        user.setFirstName(userUpdate.getFirstName());
+        user.setLastName(userUpdate.getLastName());
+        if (isAdmin) {
+            user.setRole(userUpdate.getRole());
+        }
         return userRepository.save(user);
     }
 }
